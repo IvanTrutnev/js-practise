ac1c3d27bb5cbf9247abeaca27db9fde83823034
IvanTrutnev <>
2022-09-30 17:10:55 +0300

    205-Isomorphic-strings

---
+++ 205. Isomorphic Strings/205-isomorphic-strings.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// O(n) - time, O(n) - space
+
+var isIsomorphic = function(s, t) {
+  if (s.length !== t.length) {
+      return false;
+  }
+  
+  const map = new Map();
+  
+  const set = new Set();
+  
+  for (let i = 0; i < s.length; i++) {
+      if (!map.has(s[i])) {
+          if(set.has(t[i])) {
+              return false;
+          }
+          map.set(s[i], t[i]);
+          set.add(t[i]);
+      } else if (map.get(s[i]) !== t[i]) {
+          return false;
+      }
+      
+  }
+  
+  return true;
+};
