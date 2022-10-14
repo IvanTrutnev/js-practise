4e807f2318621d58b458ab0403cba4eb67805426
Ivan <trutnev_1996@mail.ru>
2022-10-14 19:44:15 +0300

    Update 21-Merge-Two-Sorted-Lists.js

--- 21. Merge Two Sorted Lists/21-Merge-Two-Sorted-Lists.js
+++ 21. Merge Two Sorted Lists/21-Merge-Two-Sorted-Lists.js
@@ -11,22 +11,50 @@
  * @return {ListNode}
  */
 
-// O(m + n) - space, O(m + n) - time
+// [1,2,4]
+// [1,3,4]
+
+
 
 var mergeTwoLists = function(list1, list2) {
-  if (!list2) {
-    return list1;  
-  }
-  
-  if  (!list1) {
-      return list2;
-  }
-  
-  if (list1.val > list2.val) {
-      list2.next =  mergeTwoLists(list1, list2.next);
-      return list2;
-  } else {
-      list1.next = mergeTwoLists(list1.next, list2);
-      return list1;
-  }
+    if (!list1) {
+        return list2;
+    }
+    
+    if (!list2) {
+        return list1;
+    }
+    
+    if (list1.val < list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        return list2; 
+    }
 };
+
+// [1,2,4]
+// [1,3,4]
+
+// var mergeTwoLists = function(list1, list2) {
+//     let head = new ListNode();
+//     let tail = head;
+    
+//     while (list1 && list2) {
+//         if (list1.val < list2.val) {
+//             tail.next = list1;
+//             list1 = list1.next;
+//         } else {
+//             tail.next = list2;
+//             list2 = list2.next;
+//         }
+        
+//         tail = tail.next;
+//     }
+    
+//     tail.next = list1 ? list1 : list2;
+    
+//     return head.next;
+    
+// };
