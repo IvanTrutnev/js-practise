5d2e8a4217d01a23f55708267c574192e9da9f8c
Ivan Trutnev <trutnev_1996@mail.ru>
2023-09-26 23:03:39 +0300

    merge sort

--- array-sortings.js
+++ array-sortings.js
@@ -55,4 +55,38 @@ function insertionSort(arr) {
   return arr;
 }
 
-console.log(selectionSort(numbers));
+// Merge sort
+function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+
+  const length = arr.length;
+  const middle = Math.floor(length / 2);
+
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+}
+
+// Merge sort
